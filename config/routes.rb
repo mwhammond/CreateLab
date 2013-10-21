34b74d1e23b0266dc0a28b1aba41609110150cf8
Ben Miles <ben.miles2@gmail.com>
2013-10-21 01:59:56 +0100

    added contextual admin button, though doesn't lead anywhere

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Createlab::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'public#index'
-
+    
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
