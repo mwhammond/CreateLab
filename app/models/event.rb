a10e7ea466bdaea1b0bf067552b7566da7b2f093
Ben Miles <ben.miles2@gmail.com>
2013-10-27 17:20:13 +0000

    alignment of events with masonry has improved.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :event_image, :title, :starts_at, :description, :location, :partner, :external_url, :ticket_url, :event_type
   has_attached_file :event_image, 
-                    :styles => { :medium => "400x400>", :thumb => "100x100>" }, 
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
 #                     :default_url => "/images/:style/missing.png",
                     :path => ":rails_root/app/assets/images/event_images/:id/:style_:basename.:extension",
                     :url => "/assets/event_images/:id/:style_:basename.:extension"
