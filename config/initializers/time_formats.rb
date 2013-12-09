52b25d1e240e60807d1a39e21f53674390fbf5b7
Ben Miles <ben.miles2@gmail.com>
2013-12-09 01:38:29 +0000

    added pretty dates to events index

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,4 @@
 # config/initializers/time_formats.rb
+# Pretty dates from http://trevorturk.com/2010/09/23/pretty-dates-in-rails-3/
 Time::DATE_FORMATS[:month_and_year] = "%B %Y"
 Time::DATE_FORMATS[:pretty] = lambda { |time| time.strftime("%a, %b %e at %l:%M") + time.strftime("%p").downcase }
