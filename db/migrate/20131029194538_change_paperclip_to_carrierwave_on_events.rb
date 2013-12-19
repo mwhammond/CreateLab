ef8aa468569133151545f98ba2e1e80c241497a8
Ben Miles <ben.miles2@gmail.com>
2013-12-19 23:51:26 +0000

    removed the remove tasks from the db migration for removal of paperclip

--- db/migrate/20131029194538_change_paperclip_to_carrierwave_on_events.rb
+++ db/migrate/20131029194538_change_paperclip_to_carrierwave_on_events.rb
@@ -1,9 +1,9 @@
 class ChangePaperclipToCarrierwaveOnEvents < ActiveRecord::Migration
   def change
     add_column :events, :image, :string
-    remove_column :events, :event_image_file_name
-    remove_column :events, :event_image_content_type
-    remove_column :events, :event_image_file_size
-    remove_column :events, :event_image_updated_at
+#     remove_column :events, :event_image_file_name
+#     remove_column :events, :event_image_content_type
+#     remove_column :events, :event_image_file_size
+#     remove_column :events, :event_image_updated_at
   end
 end
