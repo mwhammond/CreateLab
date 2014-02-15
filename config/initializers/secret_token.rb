7fdb7c70119828c12ec089e386f5a4c95e592476
mwhammond <m.w.hammond@live.co.uk>
2014-02-15 19:20:58 +0000

    fixing gitignore issues

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,8 +10,7 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 #Createlab::Application.config.secret_key_base = (Rails.env.production? or Rails.env.staging?) ? ENV['SECRET_TOKEN'] : "a"*50
-if Rails.env.development?
-	SETTINGS = YAML.load(IO.read(Rails.root.join("config", "settings.yml")))
-	Createlab::Application.config.secret_key_base = SETTINGS['SECRET_TOKEN']
-end
+SETTINGS = YAML.load(IO.read(Rails.root.join("config", "settings.yml")))
+
+Createlab::Application.config.secret_key_base = SETTINGS['SECRET_TOKEN']
 
